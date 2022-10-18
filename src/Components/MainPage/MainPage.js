9e04c52c1edb13aeb4d2f7c9128c059d1f362dd6
francinaldapmfaria <portela854@gmail.com>
2022-10-18 13:23:46 -0300

    Exercícios realizados

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,24 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {MainContainer, Form, Input} from './styles'
 
 function MainPage() {
+
+  const [nome, setNome] = useState("")
+  const [idade, setIdade] = useState("")
+  const [email, setEmail] = useState("")
+
+  const onChangeName = (event) =>{
+    console.log("mudei o input nome")
+    console.log(event.target.value)
+    setNome(event.target.value) // seria igual nome = event.target// setNome, vai guardando o que ta sendo escrito no input
+  }
+
+  const onChangeIdade= (event) =>{
+    console.log("mudei o input idade")
+    console.log(event.target.value)
+    setIdade(event.target.value)
+  }
+
+  const onChangeEmail= (event) =>{
+    console.log("mudei o input e-mail")
+    console.log(event.target.value)
+    setEmail(event.target.value)
+
+  }
+
+    const enviarDados= () =>{
+      const formulario= [nome, idade, email]
+      console.log(formulario)
+
+    
+    setNome("")
+    setIdade("")
+    setEmail("")
+  }
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       <Form>
         <label>
           Nome:
-          <Input />
+          <Input type= "texto" onChange={onChangeName} value= {nome}placeholder= "seu nome"/>
         </label>
         <label>
           Idade:
-          <Input/>
+          <Input type= "number" onChange={onChangeIdade} value= {idade}/>
         </label>
         <label>
           E-mail:
-          <Input />
+          <Input type= "email" onChange={onChangeEmail} value= {email}/>
         </label>
-      <button>Enviar dados</button>
+      <button onClick={enviarDados}>Enviar dados</button>
       </Form>
     </MainContainer>
   )
